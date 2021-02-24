818b5958b8cb70eb27e168aeffc1156f38d94ff9
sagar <sagar@oneolabs.com>
2021-02-24 17:26:31 +0530

    updated

--- app/src/androidTest/java/com/sagar/testing/database/dao/ShoppingDaoTest.kt
+++ app/src/androidTest/java/com/sagar/testing/database/dao/ShoppingDaoTest.kt
@@ -1,20 +1,28 @@
 package com.sagar.testing.database.dao
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
+import com.google.common.truth.Truth.assertThat
+import com.sagar.testing.LiveDataTestUtil
 import com.sagar.testing.database.AppDatabase
+import com.sagar.testing.database.entity.ShoppingItem
 import org.junit.After
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-@SmallTest
 class ShoppingDaoTest {
 
-    lateinit var appDatabase: AppDatabase
+    @Rule
+    @JvmField
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var appDatabase: AppDatabase
     lateinit var shoppingDao: ShoppingDao
 
     @Before
@@ -34,7 +42,18 @@ class ShoppingDaoTest {
     }
 
     @Test
-    fun insertTest(){
+    fun insertTest() {
+        shoppingDao.add(
+            ShoppingItem(
+                name = "Apple",
+                amount = 12,
+                price = 34f
+            )
+        )
+            .blockingGet()
 
+        val items = shoppingDao.getAll().blockingFirst()
+
+        assertThat(items).isNotNull()
     }
 }
