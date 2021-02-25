5114572fb6141313f4c21f9040186421fb1eadc5
sagar <sagar@oneolabs.com>
2021-02-25 12:15:14 +0530

    live data testing util added

--- app/src/androidTest/java/com/sagar/testing/database/dao/ShoppingDaoTest.kt
+++ app/src/androidTest/java/com/sagar/testing/database/dao/ShoppingDaoTest.kt
@@ -4,9 +4,7 @@ import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.filters.SmallTest
 import com.google.common.truth.Truth.assertThat
-import com.sagar.testing.LiveDataTestUtil
 import com.sagar.testing.database.AppDatabase
 import com.sagar.testing.database.entity.ShoppingItem
 import org.junit.After
